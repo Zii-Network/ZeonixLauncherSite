dba3d00691ba3cac7b59ed4f7ee70d3c530379a3
ZinexDa <zinex283@gmail.com>
2025-12-18 21:23:01 +1000

    Translate 4

--- src/pages/FriendsPage.jsx
+++ src/pages/FriendsPage.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const FriendsPage = () => {
   return (
     <div className="section">
-      <h2>Друзья</h2>
-      <p>Здесь будет список друзей.</p>
+      <h2>Friends</h2>
+      <p>There will be a list of friends here.</p>
     </div>
   );
 };
